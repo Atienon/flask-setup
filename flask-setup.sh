5aeb92b483dee50edb62e6b730e2f1b278494e28
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-22 19:29:55 -0400

    Quick modifications

--- flask-setup.sh
+++ flask-setup.sh
@@ -7,23 +7,17 @@ source venv/bin/activate
 echo "Success"
 pip install --upgrade pip
 pip install flask
-mkdir app
-cd app
-touch __init__.py
-echo -e "from flask import Flask\n\napp = Flask(__name__)\n\nfrom app import routes" >> __init__.py
-touch routes.py
-echo -e "from app import app\n\n@app.route('/')\n@app.route('/index')\ndef index():\n  return 'Hello world'" >> routes.py
-cd ..
-touch $1.py
-echo -e "from app import app" >> $1.py
 pip install python-dotenv
-touch .flaskenv
+touch $1.py .flaskenv .env .gitignore README.md
+echo -e "from app import app" >> $1.py
 echo "FLASK_APP=$1.py" >> .flaskenv
-touch .env
+echo -e ".env\n.flaskenv\nvenv/" >> .gitignore
+mkdir app app/templates
+cd app
+touch __init__.py routes.py
+echo -e "from flask import Flask\n\napp = Flask(__name__)\n\nfrom app import routes" >> __init__.py
+echo -e "from app import app\n\n@app.route('/')\n@app.route('/index')\ndef index():\n  return 'Hello world'" >> routes.py
 pip freeze > requirements.txt
 git init
-touch .gitignore
-echo -e ".env\n.flaskenv\nvenv/" >> .gitignore
-touch README.md
 git add .
 git commit -m "First commit"
